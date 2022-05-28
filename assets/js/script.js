060f7e505ef077041075edefc2f1e3797ecfec19
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-05-28 10:44:40 -0400

    added questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,39 @@
 //Question bank
 var questionBank = [
     {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
+        question: 'Which planet in our solar system is the smallest?',
+        response: ['Earth', 'Venus', 'Pluto', 'Mercury'],
+        answer: 'Mercury'
+    },
+    {
+        question: 'Titan is the name of a moon that orbits which planet?',
+        response: ['Saturn', 'Jupiter', 'Uranus', 'Earth'],
+        answer: 'Saturn'
+    },
+    {
+        question: 'What protects Earth from meteoroids and radiation?',
+        response: ['The Atmosphere', 'The Moon', 'The Sun', 'The Asteroid Belt'],
+        answer: 'The Atmosphere'
+    },
+    {
+        question: 'How many of our planets can be seen without a telescope?',
+        response: ['3', '7', '2', '5'],
+        answer: '5'
+    },
+    {
+        question: 'How long is one year on Jupiter?',
+        response: ['5 Earth Years', '12 Earth Years', '27 Earth Years', '62 Earth Years'],
+        answer: '12 Earth Years'
+    },
+    {
+        question: 'How many moons does Earth have?',
+        response: ['2', '3', '1', '1.5'],
+        answer: '1'
+    },
+    {
+        question: 'How old is the sun?',
+        response: ['10 million years', '878 billion years', '3 billion years', '4.6 billion years'],
+        answer: '4.6 billion years'
     },
     {
         question: '',
@@ -11,43 +41,13 @@ var questionBank = [
         answer: ''
     },
     {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
+        question: 'How long does it take the suns rays to reach Earth?',
+        response: ['3 days', '1 week', 'A few hours', 'Eight minutes'],
+        answer: 'Eight minutes'
     },
     {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
-    },
-    {
-        question: '',
-        response: ['', '', '', ''],
-        answer: ''
+        question: 'Who was the first person to walk on the moon?',
+        response: ['Lance Armstrong', 'Shaquille ONeal', 'Neil Armstrong', 'Louis Armstrong'],
+        answer: 'Neil Armstrong'
     },
 ]
