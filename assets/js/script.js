5f817f488940094d077cc04be8308c7ad0ace8c4
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-05-28 10:28:50 -0400

    added questionbank layout

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//Question bank
+var questionBank = [
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+    {
+        question: '',
+        response: ['', '', '', ''],
+        answer: ''
+    },
+]
