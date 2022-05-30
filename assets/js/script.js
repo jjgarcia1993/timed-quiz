18617b5500db2c3f7b4895cf48ee2b3e6cb48073
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-05-29 22:29:06 -0400

    stuck on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //Question bank
-var questionBank = [
+const questionBank = [
     {
+        number: 1,
         question: 'Which planet in our solar system is the smallest?',
         answers: [
             'Earth',
@@ -11,6 +12,7 @@ var questionBank = [
         correct: "Mercury"
     },
     {
+        number: 2,
         question: 'Titan is the name of a moon that orbits which planet?',
         answers: [
             'Saturn',
@@ -21,6 +23,7 @@ var questionBank = [
         correct: "Saturn"
     },
     {
+        number: 3,
         question: 'What protects Earth from meteoroids and radiation?',
         answers: [
             'The Atmosphere',
@@ -31,6 +34,7 @@ var questionBank = [
         correct: "The Atmosphere"
     },
     {
+        number: 4,
         question: 'How many of our planets can be seen without a telescope?',
         answers: [
             '3',
@@ -41,6 +45,7 @@ var questionBank = [
         correct: "5"
     },
     {
+        number: 5,
         question: 'How long is one year on Jupiter?',
         answers: [
             '5 Earth Years',
@@ -51,6 +56,7 @@ var questionBank = [
         correct: "12 Earth Years"
     },
     {
+        number: 6,
         question: 'How many moons does Earth have?',
         answers: [
             '2',
@@ -61,6 +67,7 @@ var questionBank = [
         correct: "1"
     },
     {
+        number: 7,
         question: 'How old is the sun?',
         answers: [
             '10 million years',
@@ -71,6 +78,7 @@ var questionBank = [
         correct: "4.6 billion years"
     },
     {
+        number: 8,
         question: 'How many moons are currently in our solar system?',
         answers: [
             '181',
@@ -81,6 +89,7 @@ var questionBank = [
         correct: "181"
     },
     {
+        number: 9,
         question: 'How long does it take the suns rays to reach Earth?',
         answers: [
             '3 days',
@@ -91,6 +100,7 @@ var questionBank = [
         correct: "Eight minutes"
     },
     {
+        number: 10,
         question: 'Who was the first person to walk on the moon?',
         answers: [
             'Lance Armstrong',
@@ -102,125 +112,89 @@ var questionBank = [
     },
 ]
 
+// references
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerEl = document.getElementById('answer-buttons')
+const questionIndex = 0
 
-var startButton = document.getElementById('start-btn')
-var nextButton = document.getElementById('next-btn')
-var questionContainerEl = document.getElementById('question-container')
-var questionEl = document.getElementById('question')
-var answerEl = document.getElementById('answer-buttons')
-
-
-
-
-
+let shuffledQuestions
 
 
+// start button listener
+startButton.addEventListener('click', startQuiz)
+
+// start quiz function
+function startQuiz() {
+    startButton.classList.add('hide')
+    nextButton.classList.remove('hide')
+    answerEl.classList.remove('hide')
+    showQuestions()
+}
+
+// show question function
+function showQuestions(question) {
+    questionEl.innerText = questionBank.question
+    questionBank.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerEl.appendChild(button)
+    })
+}
+
+// get random question function
+for (var i = 0; i < questionBank.length; i++) {
+    var thisItem = questionBank[i];
+    answerEl.innerText = questionBank
+}
 
 
+function setNextQuestion() {
+    resetState()
+    showQuestions(shuffledQuestions[questionIndex])
+}
 
 
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild)
+    }
+}
 
+function selectAnswer(answerEl) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > questionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var answers
-// var randomQuestion
-// var questionIndex
-
-
-// startButton.addEventListener('click', startGame)
-
-// nextButton.addEventListener('click', () => {
-//     questionIndex++
-//     nextQuestion()
-// })
-
-// function startGame() {
-//     startButton.classList.add('hide')
-//     randomQuestion = questionBank.sort(() => Math.random() - .5)
-//     questionIndex = 0
-//     questionContainerEl.classList.remove('hide')
-//     answerEl.classList.remove('hide')
-
-//     showQuestion()
-// };
-
-
-// function nextQuestion() {
-//     resetState()
-//     showQuestion(randomQuestion[questionIndex])
-// }
-
-// function showQuestion(questionBank) {
-//     questionEl.innerText = questionBank[question]
-//     questionBank.answers.forEach(answer => {
-//         var button = document.createElement('button')
-//         button.innerText = answer.text
-//         button.classList.add('btn')
-//         if (answer.correct) {
-//             button.dataset.correct = answer.correct
-//         } questionBank
-//         button.addEventListener('click', selectAnswer)
-//         answerEl.appendChild(button)
-//     })
-// }
-
-// function resetState() {
-//     clearStatusClass(document.body)
-//     nextButton.classList.add('hide')
-//     while (answerEl.firstChild) {
-//         answerEl.removeChild(answerEl.firstChild)
-//     }
-// }
-
-// function selectAnswer(e) {
-//     var selectedButton = e.target
-//     var correct = selectedButton.dataset.correct
-//     setStatusClass(document.body, correct)
-//     Array.from(answerEl.children).forEach(button => {
-//         setStatusClass(button, button.dataset.correct)
-//     })
-//     if (randomQuestion.length > questionIndex + 1) {
-//         nextButton.classList.remove('hide')
-//     } else {
-//         startButton.innerText = 'Restart'
-//         startButton.classList.remove('hide')
-//     }
-// }
-
-// function setStatusClass(element, correct) {
-//     clearStatusClass(element)
-//     if (correct) {
-//         element.classList.add('correct')
-//     } else {
-//         element.classList.add('wrong')
-//     }
-// }
-
-// function clearStatusClass(element) {
-//     element.classList.remove('correct')
-//     element.classList.remove('wrong')
-// }
-
-
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
